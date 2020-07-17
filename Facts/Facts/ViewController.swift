b49e6e022e3a296afb0e6b68b2303ab491ba4499
Bilven <bilven@mobifyi.com>
2020-07-18 02:16:19 +0530

    Added basic Project files after creating new XCode Project

---
+++ Facts/Facts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Facts
+//
+//  Created by 3Embed on 18/07/20.
+//  Copyright © 2020 Bilven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
