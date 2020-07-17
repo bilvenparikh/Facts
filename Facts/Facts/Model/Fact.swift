894316820daa704a8adfd8010e99a3c040ac28ca
Bilven <bilven@mobifyi.com>
2020-07-18 02:30:24 +0530

    Removed basic ViewController File Added basic files and folders for project setup

---
+++ Facts/Facts/Model/Fact.swift
@@ -0,0 +1,13 @@
+//
+//  Fact.swift
+//  Facts
+//
+//  Created by 3Embed on 18/07/20.
+//  Copyright © 2020 Bilven. All rights reserved.
+//
+
+import UIKit
+
+class Fact: NSObject {
+
+}
---
+++ Facts/Facts/View/FactTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FactTableViewCell.swift
+//  Facts
+//
+//  Created by 3Embed on 18/07/20.
+//  Copyright © 2020 Bilven. All rights reserved.
+//
+
+import UIKit
+
+class FactTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
